a35a407219ecddefd074b1b43ec7abc9a8137d46
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-19 14:23:48 +0545

    initial commit

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <p className="text-3xl font-medium text-sky-700">
+      <Button>hello</Button>
+    </p>
+  );
+}
