00923abd0b298d2e36a0db1468037c47cc30862f
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-24 17:04:36 +0545

    implemented text editor / preview and font size

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 import React from "react";
 import ChapterTitleForm from "./_components/chapter-title-form";
+import ChapterDescriptionForm from "./_components/chapter-description-form";
 
 type ChapterPageProps = {
   params: {
@@ -75,6 +76,11 @@ const ChapterPage = async ({ params }: ChapterPageProps) => {
             </div>
             {/* chapter title form */}
             <ChapterTitleForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm
               initialData={chapter}
               courseId={params.courseId}
               chapterId={params.chapterId}
