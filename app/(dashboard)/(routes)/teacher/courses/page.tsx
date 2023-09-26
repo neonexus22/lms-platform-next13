0ed8b201670954ff6692020fe638130fd1c6670e
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-26 14:06:59 +0545

    added course table/search/dropdown edit menu

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,30 @@
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
 type Props = {};
 
-const CoursesPage = (props: Props) => {
+const CoursesPage = async (props: Props) => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/");
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create" className={buttonVariants()}>
-        New Course
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
