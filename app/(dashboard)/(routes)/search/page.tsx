a4896cbd0e27c350ec9fb1c9fdbbd19a8613bd39
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-26 15:32:40 +0545

    added category chips to browse tab and query string on click

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,20 @@
+import { db } from "@/lib/db";
+import Categories from "./_components/categories";
+
 type Props = {};
 
-const SearchPage = (props: Props) => {
-  return <div>SearchPage</div>;
+const SearchPage = async (props: Props) => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories || []} />
+    </div>
+  );
 };
 
 export default SearchPage;
