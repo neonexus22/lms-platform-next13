bc31f8f062230ab43ef07abe4523b03b5bbcc4d2
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-27 12:49:51 +0545

    added api for get-chapter

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,6 +1,23 @@
-type Props = {};
+import { getChapter } from "@/actions/get-chapter";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChapterIdPage = (props: Props) => {
+type ChapterIdPageProps = {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+};
+
+const ChapterIdPage = async ({ params }: ChapterIdPageProps) => {
+  const { courseId, chapterId } = params;
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+
+  const response = await getChapter({ userId, courseId, chapterId });
+
+  // if (!response?.chapter || !course) return redirect("/");
+
   return <div>ChapterIdPage</div>;
 };
 
